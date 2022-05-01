9a2f5c5d877baa88c0985d7d91164e12051042fa
Timothée <timotheelinocent@gmail.com>
2022-05-01 18:41:03 +0200

    minor change in urls for more lisibility.

--- comparator/urls.py
+++ comparator/urls.py
@@ -5,12 +5,18 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-    url(r'^product_detail/(?P<product_id>[0-9]+)/$',
-        views.detail, name='product_detail'),
+    url(
+        r'^product_detail/(?P<product_id>[0-9]+)/$',
+        views.detail,
+        name='product_detail'
+    ),
     url(r'^search/$', views.search),
-    url(r'^addfavorite/', views.add_favorite,
-        name='add_favorite'),
+    url(
+        r'^addfavorite/',
+        views.add_favorite,
+        name='add_favorite'
+    ),
     url(r'^favorite/', views.favorite, name='favorite'),
     url(r'^404/', views.page_not_found, name='404'),
-    url(r'^account/', views.account, name='account'),
+    url(r'^my_account/', views.my_account, name='my_account'),
 ]
