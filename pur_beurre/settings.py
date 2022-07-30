b52f487a726fb884a5e3248b42939da379bfda8a
le_shtroumpf <timotheelinocent@gmail.com>
2022-07-30 15:17:25 +0200

    change on static file for heroku

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -31,12 +31,12 @@ SECRET_KEY = os.environ.get(
 if os.environ.get('ENV') == 'PRODUCTION':
     DEBUG = False
     # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_files')
+    STATIC_URL = 'comparator/static'
+    STATIC_ROOT = os.path.join(STATIC_URL, 'staticfiles')
 
     # Extra places for collect static to find static files
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static')
+        os.path.join(BASE_DIR, 'static')
     )
     # Simplified static file serving
     # https://warehouse.python.org/project/whitenoise/
